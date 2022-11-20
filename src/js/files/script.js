5cf9ae062f1837160e542c4e8f8652a9afaa2176
krafik <alexeym4l@mail.ru>
2022-11-21 01:28:28 +0200

    end first video

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -18,16 +18,17 @@ function documentActions(e) {
 
 	if (targetElement.closest('[data-parent]')) {
 		const subMenuId = targetElement.dataset.parent ? targetElement.dataset.parent : null;
-
 		const subMenu = document.querySelector(`[data-submenu="${subMenuId}"]`);
-		const catalogMenu = document.querySelector('.catalog-header');
 		if (subMenu) {
 			const activeLink = document.querySelector('._sub-menu-active');
 			const activeBlock = document.querySelector('._sub-menu-open');
+			
 			if (activeLink && activeLink !== targetElement) {
 				activeLink.classList.remove('_sub-menu-active');
 				activeBlock.classList.remove('_sub-menu-open');
+				document.documentElement.classList.remove('sub-menu-open');
 			}
+			document.documentElement.classList.toggle('sub-menu-open');
 			// catalogMenu.classList.toggle('_sub-menu-show');
 			targetElement.classList.toggle('_sub-menu-active');
 			subMenu.classList.toggle('_sub-menu-open');
@@ -37,4 +38,24 @@ function documentActions(e) {
 		}
 		e.preventDefault();
 	}
+	if(targetElement.closest('.menu-top-header__link_catalog')){
+		// const catalogLink = targetElement.closest('.menu-top-header__link_catalog');
+		document.documentElement.classList.add('catalog-open');
+		e.preventDefault(); 
+	}
+	if(targetElement.closest('.menu-catalog__back')){
+		document.documentElement.classList.remove('catalog-open');
+
+		document.querySelector('._sub-menu-active') ? document.querySelector('._sub-menu-active').classList.remove('_sub-menu-active') : null;
+		document.querySelector('._sub-menu-open')? document.querySelector('._sub-menu-open').classList.remove('_sub-menu-open') : null;
+		// document.documentElement.classList.remove('sub-catalog-open');
+		e.preventDefault();
+	}
+	if(targetElement.closest('.sub-menu-catalog__back')){
+		document.documentElement.classList.remove('sub-menu-open');
+		document.querySelector('._sub-menu-active') ? document.querySelector('._sub-menu-active').classList.remove('_sub-menu-active') : null;
+		document.querySelector('._sub-menu-open')? document.querySelector('._sub-menu-open').classList.remove('_sub-menu-open') : null;
+		e.preventDefault();
+	}
+	
 }
